4524c4581278e0b29837a6114d725c3c1517dc85
Davran-Fr <davrangylyjowfddv@gmail.com>
2025-08-09 18:54:28 +0500

    daw

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,16 @@ import React from "react";
 import Video from "@/components/video/Video";
 import MainRecommend from "@/components/mainRecommend/MainRecommend";
 import MainPageCards from "@/components/mainPageCard/MainPageCards";
-import { clearAccess_token} from "@/features/lib/useLocaleStorage";
+import { clearAccess_token } from "@/features/lib/useLocaleStorage";
 import { clearTokenCookies } from "@/features/lib/useCookies";
-import { Accordion } from "@/components/Accoridon";
 
 const page = () => {
   // clearAccess_token()
   // clearTokenCookies()
-  
+
   return (
     <div className="w-full  bg-mainBack ">
       <Video />
-       <Accordion title="Dowran" content="dandaiwdnawindawidnawndw"/>
       <MainRecommend />
       <MainPageCards />
     </div>
--- src/provider/Providers.tsx
+++ src/provider/Providers.tsx
@@ -3,15 +3,12 @@ import { Provider } from "react-redux";
 import { store } from "../Redux/store";
 import { AnimationsProvider } from "@/components/forms/animation";
 import { UserDataBase } from "./UserDataBase";
-import GlobalLoading from "./GlobalLoading";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
       <UserDataBase>
-        <GlobalLoading>
-          <AnimationsProvider>{children}</AnimationsProvider>
-        </GlobalLoading>
+        <AnimationsProvider>{children}</AnimationsProvider>
       </UserDataBase>
     </Provider>
   );
