ad64bab97857db14567276e1f90acb9565e22a70
Davran-Fr <davrangylyjowfddv@gmail.com>
2025-10-06 00:57:21 +0500

    [added] : new one

--- src/app/account/layout.tsx
+++ src/app/account/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
 }) {
   return (
     <div className="lg:py-28 px-4 mx-auto max-w-[1100px] py-20 pt-28 font-world grid relative md:divide-x-1px divide-gray-400 grid-cols-1 md:grid-cols-5">
-      <Loading />
+      {/* <Loading /> */}
       <SideBar />
       <div className="md:col-span-4 w-full md:pl-10 min-h-[400px] h-full space-y-14">
         <BreadCrumbs name="settings" />
--- src/app/account/not-found.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NotFOunt = () => {
-  return <div className="text-5xl text-red-300">not-found</div>;
-};
-
-export default NotFOunt;
